663f31dae639b719bc8d39d446b79777305327dd
pipoche <mohamederrouhe@gmail.com>
2026-01-18 04:35:50 +0100

    start data collecting products
     Shoes for men and women are done and stored in tables of products -_-

---
+++ js/data.js
@@ -0,0 +1,72 @@
+const Man_Products = [
+{
+    id: 1,
+    title: "Men's loafer with Horsebit",
+    prix: 1090 ,
+    Description: 'Introducing Spring Summer 2026. This style is featured in the "Cocco di Mamma" and "Nerd" looks of La Famiglia. The new design puts an edgy twist on the classic Horsebit loafer, defined by a slim leather sole and reverse construction which provides a soft, comfortable and extremely flexible fit.',
+    mainVideo: "./Media/Products/m/Shoes/pro1-vid.mp4",
+    images: ["Media/Products/m/Shoes/pro1-im1.avif","Media/Products/m/Shoes/pro1-im2.avif","Media/Products/m/Shoes/pro1-im3.avif" ],
+    wOrm: "M",
+    category: "Shoes",
+
+},
+{
+    id: 2,
+    title: "Men's Stretch high-top sneaker",
+    prix: 999 ,
+    Description: "Introducing Spring Summer 2026. This style is featured in the 'Figo' look of La Famiglia. The new design reimagines the House's signature sneaker in a light, soft and flexible silhouette. Crafted from soft stretchy leather, the style has been treated by hand to achieve a worn-out appearance which makes every pair unique.",
+    mainVideo: "Media/Products/m/Shoes/pro2-vid.mp4",
+    images: ["Media/Products/m/Shoes/pro2-im-1.avif","Media/Products/m/Shoes/pro2-im-2.avif","Media/Products/m/Shoes/pro2-im-3.jpg" ],
+    wOrm: "M",
+    category: "Shoes",
+
+},
+{
+    id: 3,
+    title: "Men's Stretch low-top sneaker",
+    prix: 980 ,
+    Description: "Introducing Spring Summer 2026. This new design reimagines the House's signature sneaker in a light, soft and flexible silhouette for the La Famiglia collection. Crafted from soft stretchy leather, the style has been treated by hand to achieve a worn-out appearance which makes every pair unique.",
+    mainVideo: "Media/Products/m/Shoes/prod3-vid.mp4",
+    images: ["Media/Products/m/Shoes/pro3-im1.avif","Media/Products/m/Shoes/pro3-im2.jpg","Media/Products/m/Shoes/pro3-im3.avif" ],
+    wOrm: "M",
+    category: "Shoes",
+
+}
+
+];
+
+const Women_Products = [
+    {
+        id: 1,
+        title: "Women's Bombshell boot",
+        prix: 2300 ,
+        Description: "Introducing Spring Summer 2026. This style pays tribute to a glamorous era. Defined by a point toe and stiletto heel in gold‑toned finish, it is crafted from lightweight yet structured GG canvas in the vintage-inspired shade of sand.",
+        mainVideo: "Media/Products/wo/Shoes/Ws1-vid.mp4",
+        images: ["Media/Products/wo/Shoes/Ws1-im1.avif","Media/Products/wo/Shoes/Ws1-im2.avif","Media/Products/wo/Shoes/Ws1-im3.avif" ],
+        wOrm: "W",
+        category: "women-Shoes",
+    
+    },
+    {
+        id: 2,
+        title: "Women's Stretch low-top sneaker",
+        prix: 980 ,
+        Description: "Introducing Spring Summer 2026. This new design reimagines the House's signature sneaker in a light, soft and flexible silhouette for the La Famiglia collection. Crafted from soft stretchy leather, the style has been treated by hand to achieve a worn-out appearance which makes every pair unique.",
+        mainVideo: "Media/Products/wo/Shoes/Ws2-vid.mp4",
+        images: ["Media/Products/wo/Shoes/Ws2-im1.avif","Media/Products/wo/Shoes/Ws2-im2.avif","Media/Products/wo/Shoes/Ws2-im3.avif" ],
+        wOrm: "W",
+        category: "women-Shoes",
+    },
+    {
+        id: 3,
+        title: "Women's slingback pump with crystal Horsebit",
+        prix: 1200 ,
+        Description: "This shoe features multi-dimensional crystals as a special feature on the classic silhouette. The emblematic Horsebit adds a touch of timeless elegance, highlighting the brand's heritage. Carefully crafted, this design effortlessly combines luxury and bold style, making it a true statement piece.",
+        mainVideo: "Media/Products/wo/Shoes/Ws3-vid.mp4",
+        images: ["Media/Products/wo/Shoes/Ws3-im1.webp","Media/Products/wo/Shoes/Ws3-im2.png","Media/Products/wo/Shoes/Ws3-im3.webp" ],
+        wOrm: "W",
+        category: "women-Shoes",
+    }
+    
+
+]
